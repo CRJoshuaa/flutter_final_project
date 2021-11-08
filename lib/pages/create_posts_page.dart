f3b0ec5ae54cb87306f65255d58da493f942c49d
Joshuaa <joshuaa.chales@gmail.com>
2021-11-08 16:21:11 +0800

    Stable v2

--- lib/pages/create_posts_page.dart
+++ lib/pages/create_posts_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_final_project/states/websocket_channel_cubit.dart';
 
 class CreatePostsPage extends StatefulWidget {
   const CreatePostsPage({Key? key}) : super(key: key);
@@ -8,40 +12,67 @@ class CreatePostsPage extends StatefulWidget {
 }
 
 class _CreatePostsPageState extends State<CreatePostsPage> {
+  var postTitleController = TextEditingController();
+  var postDescriptionController = TextEditingController();
+  var postImageURLController = TextEditingController();
+  void createPost() {
+    var channel = BlocProvider.of<WebSocketChannelCubit>(context);
+
+    channel.state.stream.listen((event) {
+      var response = jsonDecode(event.toString());
+      print(response);
+    });
+
+    channel.state.sink.add(
+        '{"type": "create_post","data": {"title": "${postTitleController.text}", "description": "${postDescriptionController.text}","image": "${postImageURLController.text}"}}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create Post'),
-        centerTitle: true,
-      ),
-      body: Container(
-        child: Wrap(runSpacing: 20, children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: Icon(Icons.person),
-              hintText: 'What is the heading of the post?',
-              labelText: 'Title',
-            ),
-          ),
-          TextFormField(
-            keyboardType: TextInputType.multiline,
-            decoration: InputDecoration(
-              icon: Icon(Icons.person),
-              hintText: 'What is the heading of the post?',
-              labelText: 'Description',
-            ),
-            maxLines: 7,
-            minLines: 5,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              icon: Icon(Icons.web_asset),
-              labelText: 'Image URL',
-            ),
-          ),
-        ]),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Create Post'),
+          centerTitle: true,
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Wrap(
+                runSpacing: 20,
+                children: <Widget>[
+                  TextFormField(
+                    controller: postTitleController,
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.person),
+                      hintText: 'What is the heading of the post?',
+                      labelText: 'Title',
+                    ),
+                  ),
+                  TextFormField(
+                    controller: postDescriptionController,
+                    keyboardType: TextInputType.multiline,
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.person),
+                      hintText: 'What is the heading of the post?',
+                      labelText: 'Description',
+                    ),
+                    maxLines: 7,
+                    minLines: 5,
+                  ),
+                  TextFormField(
+                    controller: postImageURLController,
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.web_asset),
+                      labelText: 'Image URL',
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    createPost();
+                  },
+                  child: const Text('Post'))
+            ]));
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_final_project/states/websocket_channel_cubit.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -22,9 +23,7 @@ class _HomePageState extends State<HomePage> {
   var username = 'Joshuaa';
   List posts = [];
 
-  @override
-  void initState() {
-    super.initState();
+  void getPosts() {
     var channel = BlocProvider.of<WebSocketChannelCubit>(context);
 
     channel.state.stream.listen((event) {
@@ -32,14 +31,20 @@ class _HomePageState extends State<HomePage> {
 
       setState(() {
         posts = decodedMessage['data']['posts'];
+        posts = posts.reversed.toList();
       });
       print(posts);
       print(posts.length);
-
       channel.state.sink.close();
     });
 
-    channel.state.sink.add('{"type": "get_posts","data": {"limit": 10}}');
+    channel.state.sink.add('{"type": "get_posts"}');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPosts();
   }
 
   @override
@@ -47,6 +52,7 @@ class _HomePageState extends State<HomePage> {
     return BlocProvider(
       create: (context) => WebSocketChannelCubit(),
       child: Scaffold(
+        backgroundColor: Colors.purple[50],
         appBar: AppBar(
           title: Text('Welcome $username'),
           centerTitle: true,
@@ -60,8 +66,13 @@ class _HomePageState extends State<HomePage> {
         ),
         floatingActionButton: FloatingActionButton(
             onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => CreatePostsPage()));
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => BlocProvider(
+                            create: (context) => WebSocketChannelCubit(),
+                            child: CreatePostsPage(),
+                          )));
             },
             child: const Icon(Icons.create_sharp)),
         body: Center(
@@ -71,15 +82,17 @@ class _HomePageState extends State<HomePage> {
               Container(
                 padding: EdgeInsets.symmetric(horizontal: 10),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    ElevatedButton(onPressed: () {}, child: Text('Settings')),
                     //right banner
                     Container(
                       child: Row(
                         children: <Widget>[
                           ElevatedButton(
-                              onPressed: () {}, child: Icon(Icons.sort)),
+                              onPressed: () {
+                                posts = posts.reversed.toList();
+                              },
+                              child: Icon(Icons.sort)),
                           SizedBox(width: 5),
                           BlocBuilder<WebSocketChannelCubit, WebSocketChannel>(
                             builder: (context, state) {
@@ -94,46 +107,95 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
               ),
-              Text('Hello Worlds'),
+
               Container(
                 height: 600.0,
-                child: FutureBuilder(builder: (context, snapshot) {
-                  return ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      itemCount: posts.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => PostDetailsPage(
-                                          title: posts[index]['title'],
-                                          image: posts[index]['image'],
-                                        )));
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(vertical: 10),
-                            child: Row(
-                              children: <Widget>[
-                                Column(
-                                  children: [
-                                    Text(
-                                      posts[index]['author'],
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
+                child: BlocBuilder<WebSocketChannelCubit, WebSocketChannel>(
+                  builder: (context, state) {
+                    return StreamBuilder(
+                        stream: state.stream,
+                        builder: (context, snapshot) {
+                          return ListView.builder(
+                              scrollDirection: Axis.vertical,
+                              itemCount: posts.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                var dateTime = DateTime.parse(
+                                    posts[index]['date'].toString());
+                                final timeFormat = DateFormat('hh:mm a');
+                                final dateFormat = DateFormat('d/M/y');
+                                String date = dateFormat.format(dateTime);
+                                String time = timeFormat.format(dateTime);
+
+                                return InkWell(
+                                  onDoubleTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                PostDetailsPage(
+                                                  title: posts[index]['title'],
+                                                  image: posts[index]
+                                                          ['image'] ??
+                                                      "no image",
+                                                  time: time,
+                                                  date: date,
+                                                  author: posts[index]
+                                                      ['author'],
+                                                  description: posts[index]
+                                                      ['description'],
+                                                )));
+                                  },
+                                  child: Container(
+                                    margin: EdgeInsets.symmetric(
+                                        vertical: 1, horizontal: 5),
+                                    decoration:
+                                        BoxDecoration(color: Colors.white),
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 10, horizontal: 10),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Column(
+                                          children: [
+                                            Text(
+                                              posts[index]['author'],
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Text(
+                                              posts[index]['title'],
+                                            ),
+                                          ],
+                                        ),
+                                        Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            TextButton(
+                                                onPressed: () {},
+                                                child: Icon(
+                                                    Icons.favorite_outline)),
+                                            Container(
+                                              child: Column(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: [
+                                                  Text(time),
+                                                  Text(date),
+                                                ],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
                                     ),
-                                    Text(
-                                      posts[index]['title'],
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      });
-                }),
+                                  ),
+                                );
+                              });
+                        });
+                  },
+                ),
               ),
             ],
           ),
